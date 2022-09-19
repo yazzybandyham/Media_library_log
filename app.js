332107ed5198f2c4c43bee7e232b6819b683a604
Yassmin <yassminh@gmail.com>
2022-09-19 13:58:12 +0100

    feat: add child class Book & Movie to extend Media

--- app.js
+++ app.js
@@ -16,4 +16,72 @@ class Media {
     set isCheckedOut(value) {
       this._isCheckedOut = value;
     }
+
+    //toggles true or false for checked out item
+    toggleCheckOutStatus() {
+      this.isCheckedOut = !this.isCheckedOut;
+    }
+
+    //averages total ratings of item
+    getAverageRating() {
+      let ratingsSum = this.ratings.reduce((currentSum, rating) => currentSum + rating);
+      return ratingsSum / this.ratings.length;
+    }
+
+    //pushes new rating into ratings array
+    addRating(rate) {
+      return this.ratings.push(rate);
+    }
   }
+
+  class Book extends Media {
+    constructor(author, title, pages){
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+    get author() {
+      return this._author;
+    }
+    get pages() {
+      return this._pages;
+    }
+  }
+
+  class Movie extends Media {
+    constructor(director, title, runTime) {
+      super(title);
+      this._director = director;
+      this._runTime = runTime;
+    }
+    get director() {
+      return this._director;
+    }
+    get runTime() {
+      return this._runTime;
+    }
+  }
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+
+historyOfEverything.toggleCheckOutStatus();
+
+console.log(historyOfEverything.isCheckedOut)//true
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+
+console.log(historyOfEverything.getAverageRating());//4.666
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+
+speed.toggleCheckOutStatus();
+
+console.log(speed.isCheckedOut);//true
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+
+console.log(speed.getAverageRating());//2.333
